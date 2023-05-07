ee5cab220e2e1995159c3704ad8bbebdfa022006
Sahil Ratnani <sahil.ratnani@xoriant.com>
2023-05-07 11:39:40 +0530

    Initial Commit

---
+++ MyMarvel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMarvel
+//
+//  Created by Sahil Ratnani on 07/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
